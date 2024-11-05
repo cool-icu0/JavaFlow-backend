bd85d1157ff89edb45f8fb524a4bb45419c5cbda
Cool <990299103@qq.com>
2024-11-06 01:40:25 +0800

    feat："Context核心上下文定义"

---
+++ gateway-core/src/main/java/com/cool/core/Bootstrap.java
@@ -0,0 +1,18 @@
+package com.cool.core;
+
+/**
+ * API网关启动类
+ *
+ */
+public class Bootstrap
+{
+    public static void main( String[] args )
+    {
+        //加载网关静态核心配置
+        //插件初始化
+        //配置中心管理器初始化，连接配置中心，监听配置的新增、修改、删除
+        //启动容器
+        //注册中心，将注册中心的实例加载到本地
+        //服务优雅关机
+    }
+}
