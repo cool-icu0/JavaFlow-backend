3b91cc981386bad77fbe52329f9f0cd19634bf42
Cool <990299103@qq.com>
2024-11-17 23:12:54 +0800

    feat："下游SpringMVC服务实现注册中心接入"

--- gateway-client/src/main/java/com/cool/client/core/ApiProperties.java
+++ gateway-client/src/main/java/com/cool/client/core/ApiProperties.java
@@ -1,5 +1,8 @@
 package com.cool.client.core;
 
+import lombok.Data;
+
+@Data
 public class ApiProperties {
     private String registerAddress;
 
--- gateway-client/src/main/java/com/cool/client/support/AbstractClientRegisterManager.java
+++ gateway-client/src/main/java/com/cool/client/support/AbstractClientRegisterManager.java
@@ -4,16 +4,19 @@ import com.cool.client.core.ApiProperties;
 import com.cool.common.config.ServiceDefinition;
 import com.cool.common.config.ServiceInstance;
 import com.cool.gateway.register.center.api.RegisterCenter;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.ServiceLoader;
 @Slf4j
-public class AbstractClientRegisterManger {
+public class AbstractClientRegisterManager {
+
+    @Getter
     private ApiProperties apiProperties;
 
     private RegisterCenter registerCenter;
 
-    protected AbstractClientRegisterManger(ApiProperties apiProperties) {
+    protected AbstractClientRegisterManager(ApiProperties apiProperties) {
         this.apiProperties = apiProperties;
 
         //初始化注册中心对象
