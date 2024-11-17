93da9038210b69b029537165af9333f3c516ddb6
Cool <990299103@qq.com>
2024-11-17 23:20:09 +0800

    feat："下游Dubbo服务实现注册中心接入"

---
+++ gateway-client/src/main/java/com/cool/client/support/dubbo/Dubbo27ClientRegisterManager.java
@@ -0,0 +1,81 @@
+package com.cool.client.support.dubbo;
+
+import com.cool.client.core.ApiAnnotationScanner;
+import com.cool.client.core.ApiProperties;
+import com.cool.client.support.AbstractClientRegisterManager;
+import com.cool.common.config.ServiceDefinition;
+import com.cool.common.config.ServiceInstance;
+import com.cool.common.utils.NetUtils;
+import com.cool.common.utils.TimeUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.dubbo.config.spring.ServiceBean;
+import org.apache.dubbo.config.spring.context.event.ServiceBeanExportedEvent;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static com.cool.common.constants.BasicConst.COLON_SEPARATOR;
+import static com.cool.common.constants.GatewayConst.DEFAULT_WEIGHT;
+
+@Slf4j
+public class Dubbo27ClientRegisterManager extends AbstractClientRegisterManager implements ApplicationListener<ApplicationEvent> {
+
+    private Set<Object> set = new HashSet<>();
+
+    protected Dubbo27ClientRegisterManager(ApiProperties apiProperties) {
+        super(apiProperties);
+    }
+
+    @Override
+    public void onApplicationEvent(ApplicationEvent applicationEvent) {
+
+        if (applicationEvent instanceof ServiceBeanExportedEvent) {
+            try {
+                ServiceBean serviceBean = ((ServiceBeanExportedEvent) applicationEvent).getServiceBean();
+                doRegisterDubbo(serviceBean);
+            } catch (Exception e) {
+                log.error("doRegisterDubbo error", e);
+                throw new RuntimeException(e);
+            }
+        } else if (applicationEvent instanceof ApplicationStartedEvent) {
+            log.info("dubbo api started");
+        }
+    }
+
+    private void doRegisterDubbo(ServiceBean serviceBean) {
+        Object bean = serviceBean.getRef();
+
+        if (set.contains(bean)) {
+            return;
+        }
+
+        ServiceDefinition serviceDefinition = ApiAnnotationScanner.getInstance().scanner(bean, serviceBean);
+
+        if (serviceDefinition == null) {
+            return;
+        }
+
+        serviceDefinition.setEnvType(getApiProperties().getEnv());
+
+        //服务实例
+        ServiceInstance serviceInstance = new ServiceInstance();
+        String localIp = NetUtils.getLocalIp();
+        int port = serviceBean.getProtocol().getPort();
+        String serviceInstanceId = localIp + COLON_SEPARATOR + port;
+        String uniqueId = serviceDefinition.getUniqueId();
+        String version = serviceDefinition.getVersion();
+
+        serviceInstance.setServiceInstanceId(serviceInstanceId);
+        serviceInstance.setUniqueId(uniqueId);
+        serviceInstance.setIp(localIp);
+        serviceInstance.setPort(port);
+        serviceInstance.setRegisterTime(TimeUtil.currentTimeMillis());
+        serviceInstance.setVersion(version);
+        serviceInstance.setWeight(DEFAULT_WEIGHT);
+
+        register(serviceDefinition, serviceInstance);
+    }
+}
