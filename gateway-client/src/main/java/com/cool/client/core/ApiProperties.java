f1c45c23ac968a619e10e94406a909af50aa78e8
Cool <990299103@qq.com>
2024-11-17 19:41:04 +0800

    feat："客户端接入注册中心，使用SPI"

---
+++ gateway-client/src/main/java/com/cool/client/core/ApiProperties.java
@@ -0,0 +1,8 @@
+package com.cool.client.core;
+
+public class ApiProperties {
+    private String registerAddress;
+
+    private String env = "dev";
+
+}
---
+++ gateway-client/src/main/java/com/cool/client/support/AbstractClientRegisterManger.java
@@ -0,0 +1,30 @@
+package com.cool.client.support;
+
+import com.cool.client.core.ApiProperties;
+import com.cool.common.config.ServiceDefinition;
+import com.cool.common.config.ServiceInstance;
+import com.cool.gateway.register.center.api.RegisterCenter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ServiceLoader;
+@Slf4j
+public class AbstractClientRegisterManger {
+    private ApiProperties apiProperties;
+
+    private RegisterCenter registerCenter;
+
+    protected AbstractClientRegisterManger(ApiProperties apiProperties) {
+        this.apiProperties = apiProperties;
+
+        //初始化注册中心对象
+        ServiceLoader<RegisterCenter> serviceLoader = ServiceLoader.load(RegisterCenter.class);
+        registerCenter = serviceLoader.findFirst().orElseThrow(()->{
+            log.error("not found RegisterCenter impl");
+            return new RuntimeException("not found RegisterCenter impl");
+        });
+    }
+    protected void register(ServiceDefinition serviceDefinition, ServiceInstance serviceInstance) {
+        registerCenter.register(serviceDefinition, serviceInstance);
+    }
+
+}
